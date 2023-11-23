382c4f230d6098e3f65be86191c1757659658d2c
AalaaIbrahim <118854389+AalaaIbrahim@users.noreply.github.com>
2023-11-23 05:50:37 +0200

    task1: added script

---
+++ AdminLinux_Task1/script1.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+cd ~ 
+mkdir myDirectory myDirectory/secondDirectory
+touch myDirectory/secondDirectory/myNotePaper
+cp myDirectory/secondDirectory/myNotePaper myDirectory/myNotePaper
+mv myDirectory/myNotePaper myDirectory/myOldNotePaper
